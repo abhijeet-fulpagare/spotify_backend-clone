ab19412673d683e82acd8bc48f2e8fc00448a6dd
Abhijeet Fulpagare <fulpagareabhijeet39@gmail.com>
2026-07-11 23:28:32 +0530

    Music Schema and model Created

---
+++ src/models/music.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const musicSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        
+    },
+    uri: {
+        type: String,
+        required:true
+    },
+    artist: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required:true
+    }
+})
+
+const musicModel = mongoose.model("spotify_music", musicSchema);
+
+module.exports = musicModel;
