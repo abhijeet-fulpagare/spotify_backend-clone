411934031bce9bbd5b5d73455449847a740d25d4
Abhijeet Fulpagare <fulpagareabhijeet39@gmail.com>
2026-07-11 17:20:38 +0530

    Server Connects to database

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const app = require('./src/app');
+const connectDB = require('./src/db/db');
 
+
+connectDB();
 const port = process.env.PORT
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cookie_parser = require('cookie-parser');
+
+
 
 const app = express();
-
+app.use(express.json());
+app.use(cookie_parser());
 
 module.exports = app;
